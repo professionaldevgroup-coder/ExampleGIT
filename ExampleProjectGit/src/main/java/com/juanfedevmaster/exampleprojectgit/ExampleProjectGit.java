e503d56ff146971409a42a006604d5051f43429d
JuanFe Dev Master <juanfedevmaster@gmail.com>
2025-08-24 21:44:04 -0500

    First Commit project example

---
+++ ExampleProjectGit/src/main/java/com/juanfedevmaster/exampleprojectgit/ExampleProjectGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.juanfedevmaster.exampleprojectgit;
+
+/**
+ *
+ * @author jufeq
+ */
+public class ExampleProjectGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
